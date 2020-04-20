f5dddfd959644b4f1ac5d69cd8da136c1eba5458
SayanSahu99 <sayansovan@gmail.com>
2020-04-21 00:31:29 +0530

    added followers table

--- app/models.py
+++ app/models.py
@@ -6,6 +6,10 @@ from hashlib import md5
 from app import db
 from app import login 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -15,6 +19,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Posts', backref = 'author', lazy = 'dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -28,7 +37,18 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+        self.followed.append(user)
 
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -39,7 +59,7 @@ class Posts(db.Model):
     def __repr__(self):
         return "<Posts: {}>".format(self.body)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
